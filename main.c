567f7375153e472c46732614773db8e26d59c2af
Min Nguyen <nguye384@wwu.edu>
2021-11-15 19:49:34 -0800

    Fix main flag input

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
     //default specification
     int store_size = 16;
     int cache_size = 4;
-    policy_t policy = FIFO;
+    int policy = FIFO;
 
     int i=1;                        //counter to track index of argv; argv[0] = ./cache_sim
     
@@ -23,36 +23,36 @@ int main(int argc, char** argv) {
     //retrieving input from command line
     while(i < argc){
         if(argv[i][1] == 's'){
-            printf("s\n");
             if(argv[i+1] != 0){
                 store_size= atoi(argv[i+1]);      
                 i+=2;//jump to the next flag if -s has a value next to it
             }else{
                 i++;//jump to the next flag if -s doesn't have a value next to it
             }
+            printf("s %d\n", store_size);
         }else if(argv[i][1] == 'c'){
-            printf("c\n");
+            
             if(argv[i+1] != 0){
                 cache_size= atoi(argv[i+1]);
                 i+=2;
             }else{
                 i++;
             }
+            printf("c %d\n", cache_size);
         }else if(argv[i][1] == 'p'){
             printf("p %s\n", argv[i+1]);
 
-            if(argv[i+1] != 0){
-                if ((strcmp(argv[i+1],"MRU") == 0)){
-                    policy = MRU;
-                }else if ((strcmp(argv[i+1],"LRU") == 0)){
-                    policy = LRU;
-                }
-            printf("Policy: %s\n", policy);
-            i+=2;
-
+            if ((strcmp(argv[i+1],"mru") == 0)){
+                policy = MRU;
+                i+=2;
+            }else if ((strcmp(argv[i+1],"lru") == 0)){
+                policy = LRU;
+                i+=2;
             }else{
                 i++;
             }
+            printf("Policy: %d\n", policy);
+
         }else{
             printf("After while: Usage: ./cache_sim [-s storesize] [-c cachesize] [-p policy]\n");
             exit(0);
@@ -72,7 +72,7 @@ int main(int argc, char** argv) {
     printf("Enter the memory location you want to retrieve:\n");
     //read from user input till fgets encounter EOF
     while(fgets(buffer,99,stdin)!=NULL){
-        buffer[strlen(buffer)-1] = '\0';
+        //buffer[strlen(buffer)-1] = '\0';
         cache_get(&cache,atoi(buffer),&latency);
         total_latency += latency;
         memset(buffer,0,99);
